0879e0b7a7440862055cdd1d7429197c44e4bd58
developer-hyun <rlckdrkdud@naver.com>
2021-11-01 17:02:02 +0900

    11.1-3

--- src/main/java/com/example/demo/src/user/model/GetUserRes.java
+++ src/main/java/com/example/demo/src/user/model/GetUserRes.java
@@ -9,9 +9,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class GetUserRes {
-    private int userIdx;
-    private String userName;
-    private String ID;
-    private String email;
+    private String id;
     private String password;
 }
